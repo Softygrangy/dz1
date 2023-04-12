5b8f93494cf6e43199060753f2897cd21ffd7067
mynameisname <sashko9944666@gmail.com>
2023-04-12 16:04:15 +0500

    исправил ошибку

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(9);
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(5);
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine( 9 + a + 5 + b );
+}
+else
+{
+    Console.WriteLine(5 + b + a + 9);
+}
